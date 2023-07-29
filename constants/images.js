18afa9da32eb7952f4b362ba70805e36d148d740
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2023-07-29 09:53:52 +0100

    Done

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/donsoft.jpeg";
+
+export default {
+  profile,
+};
---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": '70ac684616msh1226d54380229a3p1921d6jsnb6752204aa51',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+      alert(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
